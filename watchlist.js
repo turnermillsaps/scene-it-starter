5b735bdb2e63668659088f5e13e9fb239d1f7cc8
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-03-10 14:31:36 -0400

    Modified html in template literal for watchlist.html

--- watchlist.js
+++ watchlist.js
@@ -1,7 +1,7 @@
 /*
     Current Issues:
-    - watchlistHTML seems to be empty everytime it is run
-    
+    - watchlistArr.map is getting an error saying .map is not a function
+
     OMDB API:
     - Append to all API requests
         http://www.omdbapi.com/?i=tt3896198&apikey=6ab2e908
@@ -10,24 +10,22 @@
 // Render watchlist from localStorage to page
 
 document.addEventListener('DOMContentLoaded', function(){
-    var watchlistArr = localStorage.getItem('watchlist');
+    var watchlist = localStorage.getItem('watchlist');
     // console.log(watchlistArr);
-    var watchlistHTML = [];
     function renderMovies(watchlistArr) {
-        watchlistHTML = watchlistArr.map(function(currentMovie){
+        var watchlistHTML = watchlistArr.map(function(currentMovie){
             return `
                 <div class="movie card m-2">
-                    <img class="card-img-top" src="${currentMovie.Poster}" alt="Super Sick Image">
-                    <div class="card-body">
+                    <img class="card-img-top" style="width: 300px; height: 443px;" src="${currentMovie.Poster}" alt="Super Sick Image">
+                    <div class="card-body" style="width: 300px;">
                         <h4 class="card-title">${currentMovie.Title}</h4>
                         <div class="movie-year">${currentMovie.Year}</div>
                     </div>
                 </div>
             `
         }) 
-
         return watchlistHTML.join('');
     }
 
-    document.getElementById('movies-container').innerHTML = renderMovies(watchlistHTML);
+    document.getElementById('movies-container').innerHTML = renderMovies(watchlist);
 })
